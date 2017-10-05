b77807f9c4043c499f4d1fa6b9f5eeab9a28dc8a
LeeCook1 <randoemail@email.com>
2017-10-05 04:40:46 -0500

    Natas Wargames expl

---
+++ Natas/natas28.py
@@ -0,0 +1,93 @@
+import sys
+import urllib
+import base64
+import requests
+
+SITE="http://natas28.natas.labs.overthewire.org/"
+USER="natas28"
+PWORD="JWwR438wkgTsNKBbcJoowyysdM82YjeF"
+USER_AGENT="Mozilla/5.0 (X11; U; Linux x86_64; de; rv:1.9.2.8) Gecko/20100723 Ubuntu/10.04 (lucid) Firefox/3.6.8"
+
+FILE_DICT="dictionary_natas28.txt"
+TMP="tmp.txt";
+
+BLOCK_SIZE = 16
+BLOCK_OFFSET = 10
+
+CHARS_PER_BYTE = 2
+
+HIDDEN_RIGHT_COUNT = 29
+
+KEY_SECT_OFFSET = BLOCK_SIZE * 4
+KEY_SECT_CHAR_OFFSET = KEY_SECT_OFFSET * CHARS_PER_BYTE
+
+def get_enc_str(query):
+    enc = send_query(query)
+    b64dec = base64.b64decode(enc).encode('hex')
+    return b64dec[KEY_SECT_CHAR_OFFSET: KEY_SECT_CHAR_OFFSET + BLOCK_SIZE]
+
+def send_query(query):
+    url = SITE + "?query=" + query
+    r = requests.get(url,headers={'User-Agent':USER_AGENT}, auth=(USER,PWORD))
+    encrypted_url = urllib.unquote(r.url.split('=')[1]).decode('utf8')
+    return encrypted_url
+
+def make_keys(base, found_str):
+    byte_keys = {}
+    base_url_enc = urllib.quote(base+found_str) 
+    print "Starting Keys for base:", base
+    for i in xrange(0, 256):
+        new_block_str= base_url_enc + "%%%02x" % (i)
+        enc_str = get_enc_str(new_block_str)
+        val = [i]
+        if enc_str in byte_keys.keys():
+            prev = byte_keys[enc_str]
+            val += prev
+    
+        #print "Byte:","%%%02x"%(i),"\nStr:",new_block_str,"\nEnc:",enc_str,"\nVal:",val,"\n"
+        #print "Encrpted Section:",enc_str,"Val:", val
+        sys.stdout.write("Key making progess: %d/255\r" % (i) )
+        sys.stdout.flush()
+
+        byte_keys.update( {enc_str:val} )
+
+    return byte_keys
+
+def find_byte(base, found_str):
+    choice = 0
+    byte_keys = make_keys(base, found_str)
+    print "Keys Completed"
+
+    base_enc_url = urllib.quote(base)
+    cipher_str = get_enc_str(base_enc_url)
+    byte_found = byte_keys[cipher_str]
+    found_len = len(byte_found)
+    if found_len > 1:
+        print "Found duplicate keys"
+        for i in xrange(0, found_len):
+            print "\t",i,byte_found[i]
+        
+        choice = raw_input("Pick 1:")
+        assert (choice >= 0 and choice < found_len) 
+
+    byte_found = byte_found[choice]
+    print "Byte Found:", hex(byte_found)
+
+    assert isinstance(byte_found, int), ""
+    return chr(byte_found)
+
+def main():
+    found_str = ""
+    base_str = 'A'*BLOCK_OFFSET + 'B'*16 + 'C'*15
+    
+    for i in xrange(0,  HIDDEN_RIGHT_COUNT):
+        byte = find_byte(base_str, found_str)
+        base_str = base_str[:-1]
+        found_str += byte
+        
+        print i, urllib.quote(base_str)
+
+    return 0
+
+if '__main__' == __name__:
+    main()
---
+++ Natas/natas28.sh
@@ -0,0 +1,118 @@
+
+SITE="http://natas28.natas.labs.overthewire.org/"
+#SITE="127.0.0.1:2222"
+USER="natas28"
+PWORD="JWwR438wkgTsNKBbcJoowyysdM82YjeF"
+USER_AGENT="Mozilla/5.0 (X11; U; Linux x86_64; de; rv:1.9.2.8) Gecko/20100723 Ubuntu/10.04 (lucid) Firefox/3.6.8"
+FILE_DICT="dictionary_natas28.txt"
+TMP="tmp.txt";
+
+declare -A char_encrypt_arr;
+get_encrypt()
+{
+	headers=$(curl -u "$USER":"$PWORD" -A "$USER_AGENT" "$SITE" --data-binary query="$(echo -e "$1")" -s -D - -o /dev/null);
+	#urlencode_base64_query=$(echo $headers | grep "query=" | awk -F'query=' '{print $2}');
+	#urlbase64decoded_query=$(python urldecode.py $urlencode_base64_query | xxd -p |tr -d '\n');
+	#formatted_query=$( echo $urlbase64decoded_query | sed 's/.\{32\}/&\n/g' > $TMP);
+	#line=$(sed '4q;d' $TMP);
+	echo "$headers";	
+}
+
+get_start() {
+	
+	if [ -e $FILE_DICT ] && [ $(wc -l $FILE_DICT | cut -d' ' -f1 ) -gt 0 ]
+	then
+		read -r -a left_off < $FILE_DICT;
+		start=${left_off[0]};
+		found=${left_off[1]};
+		echo $start $found;
+	else
+		echo 25 " ";
+	fi
+}
+#	if [ ! -e $FILE_DICT ]
+#	then 
+#		> $FILE_DICT
+#	else
+#		line_count=$(wc -l $FILE_DICT | cut -d' ' -f1);
+#	fi
+#
+#	if [[ $(head -n1 $FILE_DICT | tr -d ' ')  =~ ^[0-9]+$ ]]
+#	then
+#		sed -i '1d' $FILE_DICT;
+#		echo "deleted"
+#	fi
+
+make_table() 
+{
+	line_count=0;
+	char_encrypt_arr=();
+	
+	linenum=0;
+	numchars=$1;
+	found="$2";
+
+	if [ $line_count  -gt 1 ]
+	then
+		echo "Building from file: $FILE_DICT"
+
+		read -r -a cur_char < $FILE_DICT
+		while read -r -a lines
+		do
+			char_encrypt_arr+=( [${lines[0]}]=$linenum );
+			linenum=$(( $linenum+1 ))
+
+			echo "updating: char $linenum, key ${lines[0]}"
+		done < $FILE_DICT
+		echo "Finish building from file"
+	fi
+	
+	for ((i=$linenum; i < 256; i++ ))
+	do
+		charhex=$(printf '%02x' $i);
+		query=$(python -c "print 'A'*$numchars");
+		query+="$found"
+		query+="\x$charhex";
+		line=$(get_encrypt "$query");
+		char_encrypt_arr+=( [$line]=$i );
+		
+		echo "Saving: '$query' $line $i"
+	
+		echo $line $i >> $FILE_DICT ;
+	done
+
+	echo "###########################################"
+}
+
+natas28_get_encrypt()
+{
+	recent=$(get_start);
+	start=$(echo "$recent" | cut -d' ' -f1 | tr -d ' ');
+	found="$(echo "$recent" | cut -d' ' -f2 | tr -d ' ')";
+
+	echo "Starting with $start A's and found $found"
+	for (( count=$start; count > 0 ; count-- ))
+	do
+		echo "Initializing Table using $count A's..."
+		make_table $count "$found";
+		
+		query="$(python -c "print 'A'*$count")";
+		line=$(get_encrypt $query);
+
+		echo "Checking: $count A's: $query $line"	
+		if [ ${char_encrypt_arr[$line]+xyz} ]
+		then
+			char_hex=$(printf '%02x' ${char_encrypt_arr[$line]} );
+			found="$found%$char_hex"
+			echo "Found char: $char_hex, $query$found"
+
+			echo $(( $count-1 )) "$found" > $FILE_DICT;
+		else
+			echo "Can't find char for position $(( $count+1 ))";
+			exit 1;
+		fi
+	done
+}
+
+get_encrypt "$1"
+#natas28_get_encrypt
